e18e312a64e75fba56b72a48394cab359299a054
RajeenBalasuriya <rajeenbalasuriya1@gmail.com>
2025-06-08 23:50:17 +0530

    Created clean data function

--- model/main.py
+++ model/main.py
@@ -1,15 +1,26 @@
 import pandas as pd
 
+
+
+
 #Clean Data function
 def get_clean_data():
   
     data=pd.read_csv("../data/data.csv")
-    print(data.head())
+    data = data.drop(['Unnamed: 32','id'],axis=1)
+    #map values in daignosis to 1 and 0 , Malicious to 1 and other to 0
+    data['diagnosis']=data['diagnosis'].map({'M':1,'B':0})
     return data
-   
+
+
+
+
+
 
 def main():
     data=get_clean_data()
+    
+    model=create_model(data)
 
 
 if __name__ == '__main__':
