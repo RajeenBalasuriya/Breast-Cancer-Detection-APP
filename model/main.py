bfea5d07f36faacc0cb53d501b0e89aa832a4260
RajeenBalasuriya <rajeenbalasuriya1@gmail.com>
2025-06-09 01:06:57 +0530

    exported model using pcikle

--- model/main.py
+++ model/main.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,classification_report
+import pickle
 
 def create_model(data):
     x=data.drop(['diagnosis'],axis=1)
@@ -18,9 +20,12 @@ def create_model(data):
     model = LogisticRegression()
     model.fit(x_train,y_train)
 
+    #test the model
+    y_pred = model.predict(x_test)
+    print('Accuracy of breast cancer model: ',accuracy_score(y_test,y_pred))
+    print("Classification report : \n",classification_report(y_test,y_pred))
+
     return model,scaler
-
-
 
 #Clean Data function
 def get_clean_data():
@@ -31,15 +36,15 @@ def get_clean_data():
     data['diagnosis']=data['diagnosis'].map({'M':1,'B':0})
     return data
 
-
-
-
-
-
 def main():
     data=get_clean_data()
     
     model,scaler=create_model(data)
+    with open('model.pkl','wb') as f:
+        pickle.dump(model,f)
+    with open('scaler.pkl','wb') as f:
+        pickle.dump(scaler,f)    
+ 
 
 
 if __name__ == '__main__':
