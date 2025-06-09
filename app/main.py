3251dbd2fdae27397dc2e687ac84cd217f964a04
RajeenBalasuriya <rajeenbalasuriya1@gmail.com>
2025-06-09 12:14:10 +0530

    created the plot for adjusting measurements

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle 
 import pandas as pd 
+import plotly.graph_objects as go
 
 def get_clean_data():
   
@@ -47,15 +48,87 @@ def add_sidebar():
         ("Fractal dimension (worst)", "fractal_dimension_worst"),
     ]
 
+    input_dict={}
+
     for label,key in slider_labels:
-        st.sidebar.slider(
+       input_dict[key]= st.sidebar.slider(
             label,
-            min_value=float(data[key].min()),
-            max_value=float(data[key].max())
+            min_value=float(0),
+            max_value=float(data[key].max()),
+            value=float(data[key].mean())
 
         )
+    return input_dict
 
+def get_scaled_values(input_dict):
+  data = get_clean_data()
+  
+  X = data.drop(['diagnosis'], axis=1)
+  
+  scaled_dict = {}
+  
+  for key, value in input_dict.items():
+    max_val = X[key].max()
+    min_val = X[key].min()
+    scaled_value = (value - min_val) / (max_val - min_val)
+    scaled_dict[key] = scaled_value
+  
+  return scaled_dict
+  
 
+def get_radar_chart(input_data):
+    input_data=get_scaled_values(input_data)
+    categories = ['Radius', 'Texture', 'Perimeter', 'Area', 
+                    'Smoothness', 'Compactness', 
+                    'Concavity', 'Concave Points',
+                    'Symmetry', 'Fractal Dimension']
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatterpolar(
+         r=[
+          input_data['radius_mean'], input_data['texture_mean'], input_data['perimeter_mean'],
+          input_data['area_mean'], input_data['smoothness_mean'], input_data['compactness_mean'],
+          input_data['concavity_mean'], input_data['concave points_mean'], input_data['symmetry_mean'],
+          input_data['fractal_dimension_mean']
+        ],
+        theta=categories,
+        fill='toself',
+        name='Mean Value'
+    ))
+    fig.add_trace(go.Scatterpolar(
+         r=[
+          input_data['radius_se'], input_data['texture_se'], input_data['perimeter_se'], input_data['area_se'],
+          input_data['smoothness_se'], input_data['compactness_se'], input_data['concavity_se'],
+          input_data['concave points_se'], input_data['symmetry_se'],input_data['fractal_dimension_se']
+        ],
+        theta=categories,
+        fill='toself',
+        name='Standard Error'
+    ))
+
+    fig.add_trace(go.Scatterpolar(
+        r=[
+          input_data['radius_worst'], input_data['texture_worst'], input_data['perimeter_worst'],
+          input_data['area_worst'], input_data['smoothness_worst'], input_data['compactness_worst'],
+          input_data['concavity_worst'], input_data['concave points_worst'], input_data['symmetry_worst'],
+          input_data['fractal_dimension_worst']
+        ],
+        theta=categories,
+        fill='toself',
+        name='Worst Value'
+  ))
+
+    fig.update_layout(
+    polar=dict(
+        radialaxis=dict(
+        visible=True,
+        range=[0, 1]
+        )),
+    showlegend=True
+    )
+
+    return fig
 
 def main():
 
@@ -66,7 +139,7 @@ def main():
         layout="wide",
         initial_sidebar_state="expanded")
     
-    add_sidebar()
+    input_data=add_sidebar()
 
     with st.container():
         st.title("BREAST CANCER PREDICTOR")
@@ -75,7 +148,9 @@ def main():
     col1,col2 =st.columns([4,1])
 
     with col1:
-        st.write("This is column 1")
+        radar_chart=get_radar_chart(input_data)
+        st.plotly_chart(radar_chart)
+
     with col2:
         st.write("This is column 2 ")
 if __name__=='__main__':
